86c241e91fbc35b041d049a0dbc5063a4076caeb
Megan Horsfield <meganthadev@gmail.com>
2020-04-08 03:22:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :newteam
   end   
   
-  post '/team' do 
+  post '/team' do
     @name = params["name"]
     @coach = params["coach"]
     @pg = params["pg"]
@@ -15,7 +15,7 @@ class App < Sinatra::Base
     @sf = params["sf"]
     @c = params["c"]
     erb :team
-  end   
+  end 
 
 
 end
